b3323642702b7457821c6440fc3c30002d1b24aa
Mike Chan <mike@android.com>
2009-10-28 21:14:12 -0700

    msm: cpufreq: Do not call a cpu transition if selecting the same speed.
    Signed-off-by: Mike Chan <mike@android.com>

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -96,6 +96,9 @@ static int msm_cpufreq_target(struct cpufreq_policy *policy,
 		return -EINVAL;
 	}
 
+	if (policy->cur == table[index].frequency)
+		return 0;
+
 #ifdef CONFIG_CPU_FREQ_DEBUG
 	dprintk("CPU[%d] target %d relation %d (%d-%d) selected %d\n",
 		policy->cpu, target_freq, relation,
