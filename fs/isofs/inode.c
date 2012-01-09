8e61309de6c9da02210e0e879e706fe329fc523e
Al Viro <viro@zeniv.linux.org.uk>
2012-01-09 10:48:11 -0500

    isofs: inode leak on mount failure
    d_alloc_root() failure leaves root inode leaked...
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    
    Conflicts:
    
    	fs/isofs/inode.c
    
    Backported from Linux 3.2 by John Kenney

--- fs/isofs/inode.c
+++ fs/isofs/inode.c
@@ -883,8 +883,11 @@ root_found:
 
 	/* get the root dentry */
 	s->s_root = d_alloc_root(inode);
-	if (!(s->s_root))
-		goto out_no_root;
+	if (!(s->s_root)) {
+		iput(inode);
+		error = -ENOMEM;
+		goto out_no_inode;
+	}
 
 	table = 0;
 	if (joliet_level)
