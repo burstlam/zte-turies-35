e960a69ed354b8bccc7b7040d493300a1a62fc88
John Kenney <johnkenney02@gmail.com>
2012-01-11 09:25:17 +0000

    Revert "msm: cpufreq: Do not call a cpu transition if selecting the same speed."
    This reverts commit b3323642702b7457821c6440fc3c30002d1b24aa.

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -96,9 +96,6 @@ static int msm_cpufreq_target(struct cpufreq_policy *policy,
 		return -EINVAL;
 	}
 
-	if (policy->cur == table[index].frequency)
-		return 0;
-
 #ifdef CONFIG_CPU_FREQ_DEBUG
 	dprintk("CPU[%d] target %d relation %d (%d-%d) selected %d\n",
 		policy->cpu, target_freq, relation,
